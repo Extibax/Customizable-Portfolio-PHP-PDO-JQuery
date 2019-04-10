b84f71171a927b923b8f0b13f604148df2098e1c
Extibax <extibax@gmail.com>
2019-04-10 16:31:37 -0500

    Insertion of data super reduced!

---
+++ php/insert_initial_content.php
@@ -0,0 +1,161 @@
+<?php
+
+session_start();
+
+require 'connection.php';
+
+if (/* $_GET['send'] == 'true' && isset($_SESSION['User']) */ true) {
+
+    if (isset($_POST['submit'])/*  && isset($_SESSION['User']) */) {
+        echo '<br>Im preparing to save data<br>';
+
+        $dbh = new PDO('mysql:host=localhost;dbname=eam_consultores', 'root', '', [
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ERRMODE_WARNING
+        ]);
+
+        $about_image_1 = $_FILES['about_image_1'];
+
+        $portfolio_image_1 = $_FILES['portfolio_image_1'];
+
+        $portfolio_image_2 = $_FILES['portfolio_image_2'];
+
+        $portfolio_image_3 = $_FILES['portfolio_image_3'];
+
+        $user_id = $_SESSION['User']['ID'];
+
+        $content = array(
+            $landing_subtitle = "Contabilidad | Administracion | Reclutamiento",
+
+            $about_description = 'Somos un equipo de profesionales, nuestro objetivo primordial es brindar <b>"una solucion integral"</b> de
+            calidad que optimice la <b>eficiencia</b> y productividad de tu negocio, ofreciendo servicios en manejo de
+            <b>Talento Humano</b>, <b>Outsourcing de planilla</b>, <b>Contabilidad administrativa</b>, <b>Contabilidad Financiera</b>,
+            <b>Contabilidad Tributaria</b> entre otros que nos convierten en un verdadero centro de operaciones
+            empresariales.
+            <br><br>
+            Ponemos a disposicion todos estos servicios, producto de nuestro compromiso, seriedad,
+            confidencialidad, creatividad y trabajo en equipo',
+
+            $social_media_links = json_encode(array(
+                "Facebook" => "facebook.com",
+                "Instagram" => "instagram.com",
+                "Whatsapp" => "whatsapp.com"
+            )),
+
+            $services_description = json_encode(array(
+                "Contabilidad" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                "Administracion" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                "Reclutamiento" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            )),
+
+            $portfolio_titles = json_encode(array(
+                "F_title" => "Contabilidad",
+                "S_title" => "Administracion",
+                "T_title" => "Reclutamiento"
+            )),
+
+            $portfolio_subtitles = json_encode(array(
+                "F_subtitle" => "Smartmatic",
+                "S_subtitle" => "OMNI.PRO",
+                "T_subtitle" => "Rootstack"
+            )),
+
+            $portfolio_descriptions = json_encode(array(
+                "F_description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                "S_description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                "T_description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            )),
+
+            $portfolio_links = json_encode(array(
+                "F_link" => "https://www.smartmatic.com/",
+                "S_link" => "https://omni.pro/",
+                "T_link" => "https://www.rootstack.com/es"
+            )),
+
+            $about_image_type_1 = json_encode(array(
+                "Name" => $about_image_1['name'],
+                "Type" => $about_image_1['type']
+            )),
+
+            $portfolio_image_type_1 = json_encode(array(
+                "Name" => $portfolio_image_1['name'],
+                "Type" => $portfolio_image_1['type']
+            )),
+
+            $portfolio_image_type_2 = json_encode(array(
+                "Name" => $portfolio_image_2['name'],
+                "Type" => $portfolio_image_2['type']
+            )),
+
+            $portfolio_image_type_3 = json_encode(array(
+                "Name" => $portfolio_image_3['name'],
+                "Type" => $portfolio_image_3['type']
+            )),
+
+            $about_image_file_1 = file_get_contents($about_image_1['tmp_name']),
+
+            $portoflio_image_file_1 = file_get_contents($portfolio_image_1['tmp_name']),
+
+            $portoflio_image_file_2 = file_get_contents($portfolio_image_2['tmp_name']),
+
+            $portoflio_image_file_3 = file_get_contents($portfolio_image_3['tmp_name'])
+        );
+
+        $question_marks = array_map(function () {
+            return ", ?";
+        }, $content);
+
+        $question_marks_reduced = ltrim(array_reduce($question_marks, function ($a, $b) {
+            return $a . " " . $b;
+        }), " ,");
+
+        $stmt = $dbh->prepare("INSERT INTO content VALUES ( null, $user_id, $question_marks_reduced )");
+
+        array_map(function ($cont, $index) use ($stmt) {
+            $stmt->bindValue($index + 1, $cont);
+        }, $content, array_keys($content));
+
+        echo $stmt->execute() ? 'Success' : 'Error: ' . $stmt->errorInfo();
+    }
+} else {
+    header("Location: ../views/login.html");
+}
+
+/* $corp_resp_template = [0.4, 0.2, 0.1];
+$corp_resp = [[0.2, 0.3, 0.5], [0.2, 0.5, 0.7]];
+
+for ($i = 0; $i < count($corp_resp); $i++) {
+
+    $total[$i] = (array_reduce(array_map(function ($x, $y) {
+        return $x * $y;
+    }, $corp_resp[$i], $corp_resp_template), function ($carry, $item) {
+        return $carry += $item;
+    }, 0));
+}
+
+print $total; */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Insert Content</title>
+</head>
+
+<body>
+    <form method="POST" enctype="multipart/form-data">
+        <input type="file" name="about_image_1">
+        <input type="file" name="portfolio_image_1">
+        <input type="file" name="portfolio_image_2">
+        <input type="file" name="portfolio_image_3">
+        <input type="submit" name="submit" value="Upload">
+    </form>
+</body>
+
+</html>
